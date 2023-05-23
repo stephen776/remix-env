c5d530a7533093ea86593a5ec5c8104625f32cd8
Steve Jacobs <sjacobs@truefit.io>
2023-05-23 10:40:38 -0400

    start building out the createEnv function

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,39 @@
-export const test = { test: 'hello' };
+import z, { ZodObject, type ZodType } from 'zod';
 
-export function foo(): void {
-  const thing = 'test';
-  console.log(thing);
+export type Options<T extends Record<string, ZodType>> = {
+  env: T;
+};
+
+export function createEnv<
+  T extends Record<string, ZodType> = NonNullable<unknown>
+>(opts: Options<T>): z.infer<ZodObject<T>> {
+  const schema = z.object(typeof opts.env === 'object' ? opts.env : {});
+
+  // given the keys in our schema, get the values from process.env
+  const envirmonment = Object.keys(schema.shape).map((key) => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-return
+    return process.env[key];
+  });
+
+  const parsed = schema.safeParse(envirmonment);
+
+  if (parsed.success === false) {
+    console.error(
+      '❌ Invalid environment variables:',
+      parsed.error.flatten().fieldErrors
+    );
+
+    throw new Error('Invalid envirmonment variables');
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-explicit-any
+  return parsed.data as any;
 }
+
+const test = createEnv({
+  env: {
+    TEST_VARIABLE: z.string()
+  }
+});
+
+console.log({ test });
